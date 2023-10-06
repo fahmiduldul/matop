29b6cf2ef07828ca3579ebd1f57fcb5f97a28236
fahmiduldul <afahmi13@live.com>
2023-10-06 15:38:40 +0700

    fix: printf

--- src/handler.h
+++ src/handler.h
@@ -1,8 +1,10 @@
+#include <cstdio>
+
 static void HandleError( cudaError_t err,
                         const char *file,
                         int line ) {
   if (err != cudaSuccess) {
-    printf( "%s in %s at line %d\n", cudaGetErrorString( err ),
+    std::printf( "%s in %s at line %d\n", cudaGetErrorString( err ),
            file, line );
     exit( EXIT_FAILURE );
   }
